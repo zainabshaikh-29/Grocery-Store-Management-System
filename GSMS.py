894f86fef9e34edb6deb7c7da45e3b2484055784
zainabshaikh-29 <zainabshaikh1722@gmail.com>
2026-09-07 11:02:41 +0530

    v1.0 - purchase and return grocery items

---
+++ GSMS.py
@@ -0,0 +1,8 @@
+# Grocery Store Management System - Version 1.0
+# Feature: purchase and return grocery items
+
+def purchase_item(item_id, customer_id):
+    print("Item", item_id, "purchased by customer", customer_id)
+
+def return_item(item_id):
+    print("Item", item_id, "returned")
